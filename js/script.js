462c4a93e7c041ff44fe8045955938170f9a3d4b
Xuan <email.xhuang@gmail.com>
2020-03-15 21:36:51 -0400

    Add hamburger toggling menu for mobile view

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function(){
+// 	// console.log('finished loading');
+// });
+
+
+var burger = document.getElementById('burger'),
+	navMenu = document.getElementById('nav-overlay');
+
+burger.onclick = function() {
+	navMenu.classList.toggle('open');
+}
+
+// burger.addEventListener('click', function(){
+// 	// console.log('clicked burger');
+// 	navMenu.toggleClass('open');
+//
+// 	classList
+// });
