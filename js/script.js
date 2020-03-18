456a45d91fc19ea100647ea936c37c5c95d2797d
Xuan <email.xhuang@gmail.com>
2020-03-18 17:34:47 -0400

    Delete commented stuff

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
 	// console.log('finished loading');
-
 	var burger = document.getElementById('burger'),
 		navMenu = document.getElementById('nav-overlay');
 
@@ -8,16 +7,5 @@ document.addEventListener('DOMContentLoaded', function(){
 		navMenu.classList.toggle('open');
 	}
 
-
 	document.getElementById('current-year').innerText = new Date().getFullYear();
 });
-
-
-
-
-// burger.addEventListener('click', function(){
-// 	// console.log('clicked burger');
-// 	navMenu.toggleClass('open');
-//
-// 	classList
-// });
