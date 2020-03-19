61f890cfa8b8a711a62c510b40a27a3e29d5e388
Xuan <email.xhuang@gmail.com>
2020-03-18 21:51:28 -0400

    Add bouncing arrow animation for mobile and tablet views to indicate scroll

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
 	// console.log('finished loading');
 	var burger = document.getElementById('burger'),
-		navMenu = document.getElementById('nav-overlay');
+		navMenu = document.getElementById('nav-overlay'),
+		arrowContainer = document.getElementById("bouncing-arrow-container");
 
 	burger.onclick = function() {
 		navMenu.classList.toggle('open');
 	}
+
+	// written with help from:
+	// https://jsfiddle.net/cvmw3L1o/1/
+	document.addEventListener("scroll", (e) => {
+		var scrolledDistance = document.scrollingElement.scrollTop;
+
+		if (scrolledDistance > 250) {
+			arrowContainer.classList.add('hide');
+		} else {
+			arrowContainer.classList.remove('hide');
+		}
+	});
 
 	document.getElementById('current-year').innerText = new Date().getFullYear();
 });
